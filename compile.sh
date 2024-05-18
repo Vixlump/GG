4c3a8580d487a0ee437d6e7df4086db91a75b898
Vixlump <vixlump@lumpology.com>
2024-05-18 14:23:11 -0700

    Initial commits

---
+++ compile.sh
@@ -0,0 +1,3 @@
+g++ dev.cpp -std=c++20 -pedantic -O2 -o GG_Server
+echo Done!
+
---
+++ dev.cpp
@@ -0,0 +1,30 @@
+//this
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <vector>
+#include <deque>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <algorithm>
+#include <filesystem>
+#include <functional>
+#include <map>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "libs/time_tools.hpp"
+#define loop while(true)
+
+namespace gg{
+
+}
