1f23e9c67f02ac2bd9c698a026f5c1d5b0dfec35
Vixlump <vixlump@lumpology.com>
2024-05-18 16:58:50 -0700

    lots of stuff

--- dev.cpp
+++ dev.cpp
@@ -25,22 +25,104 @@
 //#include "libs/time_tools.hpp"
 #define loop while(true)
 
+//#define PORT 25588
+//#define BUFFER_SIZE 1024
+
 namespace gg{
-	void server_listening_loop() {
+	void server_listening_loop(){
 		loop {
 
 		}
 	}
 	void server_clean_system() {
-		std::cout<<
+		std::cout<<"System Closing..."<<std::endl;
 	}
 	void server_store_file_driver(std::string API_KEY, std::string) {
 
 	}
 }
 
+int dever() {
+	constexpr size_t BUFFER_SIZE = 1024;
+	constexpr int PORT = 25588;
+
+	std::string message = "confirm_file_recived";
+
+    int server_fd;
+    int new_socket;
+    struct sockaddr_in address;
+    struct sockaddr_in client_addr;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+	socklen_t client_addrlen = sizeof(client_addr);
+
+    //socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cout<<"socket failed\n";
+        return (EXIT_FAILURE);
+    }
+
+    //bind the socket to the port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        std::cout<<"bind failed\n";
+        close(server_fd);
+        return (EXIT_FAILURE);
+    }
+
+    //listen for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        std::cout<<"listen\n";
+        close(server_fd);
+        return (EXIT_FAILURE);
+    }
+
+    std::cout << "Server listening on port: " << PORT << std::endl;
+
+    //accept an incoming connection
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        std::cout<<"accept\n";
+        close(server_fd);
+        return (EXIT_FAILURE);
+    }
+
+    //ppen a file to save the received data
+    std::ofstream outfile("archive/temp.GIF", std::ios::binary);
+    if (!outfile) {
+        std::cout<<"file open\n";
+        close(new_socket);
+        close(server_fd);
+        return (EXIT_FAILURE);
+    }
+
+    //read the data from the socket and write it to the file
+    int bytes_read;
+    while ((bytes_read = read(new_socket, buffer, BUFFER_SIZE)) > 0) {
+        outfile.write(buffer, bytes_read);
+    }
+
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+    std::cout << "File received from<"<<client_ip << ":" << ntohs(client_addr.sin_port) <<"> and saved as 'temp.GIF'" << std::endl;
+
+    //send return message to client
+    send(new_socket, message.c_str(), strlen(message.c_str()), 0);
+
+    //clean up
+    outfile.close();
+    close(new_socket);
+    close(server_fd);
+
+    return EXIT_SUCCESS;
+}
+
 auto main (int argc, char ** argv) -> int {
-	gg::server_listening_loop();
+	//gg::server_listening_loop();
+	int result = dever();
+	std::cout<<"Result: "<<result<<std::endl;
 	gg::server_clean_system();
-	return EXIT_SUCCESS;
+	return result;
 }
--- testing.cpp
+++ testing.cpp
@@ -22,9 +22,72 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#include "libs/time_tools.hpp"
 #define loop while(true)
 
 namespace gg_test{
 
+}
+
+int main() {
+	std::string ADDRESS = "127.0.0.1";
+	constexpr size_t BUFFER_SIZE = 1024;
+	constexpr int PORT = 25588;
+
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+
+    //create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cout << "Socket creation error" << std::endl;
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    //convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, ADDRESS.c_str(), &serv_addr.sin_addr) <= 0) {
+        std::cout << "Invalid address/ Address not supported" << std::endl;
+        return -1;
+    }
+
+    //connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cout << "Connection Failed" << std::endl;
+        return -1;
+    }
+
+    //open the file to be sent
+    std::ifstream infile("temp.GIF", std::ios::binary);
+    if (!infile) {
+        std::cout << "File open error" << std::endl;
+        close(sock);
+        return -1;
+    }
+
+    //read from the file and send the data to the server
+    while (infile.read(buffer, BUFFER_SIZE)) {
+        send(sock, buffer, infile.gcount(), 0);
+    }
+
+    //send any remaining data
+    if (infile.gcount() > 0) {
+        send(sock, buffer, infile.gcount(), 0);
+    }
+
+    std::cout << "File sent successfully" << std::endl;
+
+    //this segment here causes a segfault fix later
+    // //receive confirmation message from server
+    // int bytes_received = read(sock, buffer, BUFFER_SIZE);
+    // if (bytes_received > 0) {
+    //     std::cout << "Server said: " << std::string(buffer, bytes_received) << std::endl;
+    // }
+
+    //clean up
+    infile.close();
+    close(sock);
+
+    return 0;
 }
