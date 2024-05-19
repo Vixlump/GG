061c52af54d4aad7fb498148a7f7636a00726f2c
Saarang <88927866+saaranganand@users.noreply.github.com>
2024-05-19 05:24:58 -0700

    Delete menu/build directory

--- menu/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,3 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
-  /Users/saaranganand/Desktop/stormhack/GG/menu/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
-  /Users/saaranganand/Desktop/stormhack/GG/menu/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
--- menu/build/_deps/ftxui-build/CMakeFiles/screen.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libftxui-screen.a"
-)
