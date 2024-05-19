ba6e4faedaf93d3d2c4078aad9bccdbcb4e409e3
Micah Baker <micahb923@gmail.com>
2024-05-18 22:18:05 -0700

    Merge branch 'client' into branch 'main' (manual)

---
+++ client/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+
+extern "C" {
+	#include <libavutil/version.h>
+}
+
+#include "gg.hpp"
+
+#define GG_HELP\
+	"gg - v0.1 - [g]leeble[g]lob command line interface.\n"\
+	"Check out https://gleebleglob.club for service information.\n"\
+	"GENERAL USAGE:\n"\
+	"\tgg [command] [...arguments]\n"\
+	"\tNOTE: excess arguments will be ignored.\n"\
+	"COMMANDS:\n"\
+	"\tgg help\n"\
+	"\tgg register [username]\n"\
+	"\tgg login [username]\n"\
+	"\tgg logout\n"\
+	"\tgg status\n"\
+	"\tgg upload [file path] [password]\n"\
+	"\tgg stream [file token] [password]"
+
+#define GG_INSUFFICIENT_ARGUMENTS  GG_HELP "\n--\nInsufficient arguments."
+
+int main(int argc, char **argv) {
+	// if no arguments provided, assume 'help' command
+	if (argc == 1) {
+		std::cout << GG_HELP << std::endl;
+		return 1;
+	}
+
+	// gleebleglob client
+	GG::client client;
+
+	// parse command from arguments
+	std::string cmd = argv[1];
+
+	// help command
+	if (cmd == "help") {
+		std::cout << GG_HELP << std::endl;
+		return 0;
+	// gg register [username]
+	} else if (cmd == "register") {
+		if (argc != 3) {
+			std::cout << GG_INSUFFICIENT_ARGUMENTS << std::endl;
+			return 1;
+		}
+
+		std::string username = argv[2];
+		return client.request(username);
+	// gg login [username]
+	} else if (cmd == "login") {
+		if (argc != 3) {
+			std::cout << GG_INSUFFICIENT_ARGUMENTS << std::endl;
+			return 1;
+		}
+
+		std::string username = argv[2];
+		return client.login(username);
+	// gg logout
+	} else if (cmd == "logout") {
+		return client.logout();
+	// gg status
+	} else if (cmd == "status") {
+		std::cout << client.status() << std::endl;
+		return 0;
+	// gg upload [file path] [password]
+	} else if (cmd == "upload") {
+		if (argc != 4) {
+			std::cout << GG_INSUFFICIENT_ARGUMENTS << std::endl;
+			return 1;
+		}
+
+		std::string file_path = argv[2];
+		std::string password = argv[3];
+		return client.upload(file_path, password);
+	// gg stream [file token] [password]
+	} else if (cmd == "stream") {
+		if (argc != 4) {
+			std::cout << GG_INSUFFICIENT_ARGUMENTS << std::endl;
+			return 1;
+		}
+
+		std::string file_token = argv[2];
+		std::string password = argv[3];
+		return client.stream(file_token, password);
+	} else {
+		std::cout << GG_HELP << "\n--\nDon't know how to \"" << cmd << "\"." << std::endl;
+		return 1;
+	}
+
+	// exec path shouldn't get here anyways
+	return 0;
+}
