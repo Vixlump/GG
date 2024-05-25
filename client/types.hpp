c48feca44d024d630760bb003ddaf35f4e9242c1
Gabe WSL Debian <24978329+EarthenSky@users.noreply.github.com>
2024-05-24 20:39:21 -0700

    switch to consistent naming of color

--- client/types.hpp
+++ client/types.hpp
@@ -8,12 +8,12 @@
 // the formatting needed to describe a single unit of the terminal screen. 
 // "a pixel", but not. "a character", but not. both!
 struct CharFormat {
-	uint8_t fg_color_index; // this is an index into the xterm_colours array
+	uint8_t fg_color_index; // this is an index into the xterm_colors array
 	uint8_t bg_color_index;
 	uint8_t char_index;
 };
 
-// The parameters to minimze for computing the colour lookup table
+// The parameters to minimze for computing the color lookup table
 enum ColorSchemeKind {
     Arbitrary, MSE, Contrast
 };
@@ -21,8 +21,8 @@ enum ColorSchemeKind {
 // contains all the buffers needed to render a screen.
 //struct RenderableScreenBuffer {
 //	std::string char_buffer;
-//	std::vector<ftxui::Color> fg_colour_buffer;
-//	std::vector<ftxui::Color> bg_colour_buffer;
+//	std::vector<ftxui::Color> fg_color_buffer;
+//	std::vector<ftxui::Color> bg_color_buffer;
 //};
 
 #endif // TYPES_H
